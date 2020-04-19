4ddf0092066c8359b9bda07d32d3b59699496e22
yasserangelgm <yasserangelgm@gmail.com>
2020-04-19 07:51:54 -0500

    Create sort-algorithms.html and obtainDivArray function
    Se movio el area de demostración de losalgoritmos a la página correspondiente, además de el cambio de nombres de funciones y variables. Se creó también una función para obtener un array de los nodos de cada una de las barras.

--- js/scripts.js
+++ js/scripts.js
@@ -11,7 +11,7 @@ var colorPalette = [
   "#216334",
   "#C2E2A7",
   "#556F25",
-  "#D6EECD",
+  "#D6EE45",
   "#ABE3D2",
   "#D9D98C",
   "#8585D6",
@@ -81,23 +81,51 @@ function FisherYatesShuffle() {
 }
 
 function createBar(width, value) {
+  //Create the div for the bar and the number box
+  var barContainer = document.createElement("div");
+  barContainer.setAttribute("class", "barContainer");
+  barContainer.setAttribute("id", value);
+
+  //create the bar div
   var bar = document.createElement("div");
   var height = value * 6;
   bar.style.width = width;
   bar.style.height = height.toString() + "px";
   bar.style.backgroundColor = colorPalette[value - 1];
   bar.setAttribute("class", "bar");
-  return bar;
+  //create the number box
+  var numBox = document.createElement("div");
+  numBox.setAttribute("class", "numberBox");
+  numBox.style.outlineColor = colorPalette[value - 1];
+  numBox.style.color = colorPalette[value - 1];
+  var number = document.createTextNode(value.toString());
+  numBox.appendChild(number);
+  barContainer.appendChild(bar);
+  barContainer.appendChild(numBox);
+  return barContainer;
 }
 
-function makeBars(values) {
-  var barContainer = document.querySelector("#barContainer");
+function placeBarsInDemoArea(values) {
+  var barsArea = document.querySelector("#demoArea");
   var shuffleBar;
   for (var i = 0; i < values.length; i++) {
     shuffleBar = createBar("40px", values[i]);
-    barContainer.appendChild(shuffleBar);
+    barsArea.appendChild(shuffleBar);
   }
 }
 
 var shuffledArray = FisherYatesShuffle();
-makeBars(shuffledArray);
+placeBarsInDemoArea(shuffledArray);
+
+function obtainDivArray() {
+  var bars = document.querySelector("#demoArea");
+  var nodeList = bars.childNodes;
+  var array = [];
+  for (var i = 0; i < nodeList.length; i++) {
+    array[i] = nodeList[i];
+  }
+  return array;
+}
+
+var divArray = obtainDivArray();
+console.log(divArray);
